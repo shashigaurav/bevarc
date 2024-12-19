f13497e97abcbdae4efb1d42993e61a4744ef54c
Shashi Gaurav <shashigaurav789@gmail.com>
2024-12-19 15:42:54 +0530

    removed next-themes

--- components/ui/sonner.jsx
+++ components/ui/sonner.jsx
@@ -1,15 +1,13 @@
 "use client";
-import { useTheme } from "next-themes"
 import { Toaster as Sonner } from "sonner"
 
 const Toaster = ({
   ...props
 }) => {
-  const { theme = "system" } = useTheme()
 
   return (
     (<Sonner
-      theme={theme}
+      theme={'dark'}
       className="toaster group"
       toastOptions={{
         classNames: {
